0642ef1d91d1e1711c108e3e5c30ba739e08168f
bhupendra balapure <bhupendrabalapure@gmail.com>
2023-08-21 17:01:36 +0530

    user page best selling and top product

--- app/Http/Livewire/HomeComponent.php
+++ app/Http/Livewire/HomeComponent.php
@@ -11,7 +11,9 @@ class HomeComponent extends Component
     public function render()
     {
         $products = Product::all()->unique('name');
+        $top_products = Product::where('top_product','1')->get()->unique('name');
+        $best_products = Product::where('best_product','1')->get()->unique('name');
         $categorys = Category::all();
-        return view('livewire.home-component',['products'=>$products,'categorys'=>$categorys])->layout('layouts.base');
+        return view('livewire.home-component',['products'=>$products,'categorys'=>$categorys,'top_products'=>$top_products,'best_products'=>$best_products])->layout('layouts.base');
     }
 }
